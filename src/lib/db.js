4052269dfc78931ed748adece123ff53d3cfd6fd
Todor Markov <todor.markoww@gmail.com>
2021-12-28 12:09:18 +0200

    second-commit

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY,
-  {
-    fetch: (...args) => fetch(...args),
-}
-)
+
+const supabaseUrl = 'https://nphykmdyafqximjeemqm.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDE2MzE1MCwiZXhwIjoxOTU1NzM5MTUwfQ.-w1NRh4b-iPxprky42PiYQ211LewXV_hiIJ8GrB7kNY'
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
