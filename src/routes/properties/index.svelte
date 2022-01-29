407ae1688b4b34e7c5b5efbef0931b5d23d21616
Todor Markov <todor.markoww@gmail.com>
2022-01-29 20:02:05 +0200

    swipperr

--- src/routes/properties/index.svelte
+++ src/routes/properties/index.svelte
@@ -32,29 +32,63 @@
 <svelte:head>
 	<title>Оферти</title>
 </svelte:head>
+
+
 <script>
 
-   import CardProperties from '../../Components/cardProperties.svelte';
- 
+  //  import CardProperties from '../../Components/cardProperties.svelte';
+ import { Swiper, SwiperSlide } from 'swiper/svelte';
+ import SwiperCore, {
+  Lazy ,Navigation
+} from 'swiper';
+import 'swiper/css';
 
+import "swiper/css/lazy"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
  
   export let data;
-   
+     if(data)
+  SwiperCore.use([Lazy ,Navigation]);
 </script>
-
-<div class="grid grid-cols-3 gap-8 pt-10">
  
+ 
+   <Swiper style="--swiper-navigation-color: #fff;--swiper-pagination-color: #fff;   height: 30rem;width:100%"   lazy="{true}"  navigation="{true}" class="mySwiper">
 {#each data as data_offer}
  
-  
- <CardProperties {data_offer}/>  
+  <SwiperSlide>
+          <!-- Required swiper-lazy class and image source specified in data-src attribute -->
+          <img  data-src={data_offer.image} class="swiper-lazy" />
+          <!-- Preloader image -->
+          <div class="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+        </SwiperSlide>
+ <!-- <CardProperties {data_offer}/>   -->
  
 {/each}
 
+   </Swiper>
  
-</div>
+
 <style>
- 
   
- 
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #000;
+}
+
+.swiper-slide img {
+  width: auto;
+  height: auto;
+  max-width: 100%;
+  max-height: 100%;
+  -ms-transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  -moz-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  position: absolute;
+  left: 50%;
+  top: 50%;
+}
 </style>
