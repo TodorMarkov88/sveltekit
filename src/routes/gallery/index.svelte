7918b5378be6c2c259112e02ca7a45cebc108050
Todor Markov <todor.markoww@gmail.com>
2022-02-05 19:57:57 +0200

    fix-properties

--- src/routes/gallery/index.svelte
+++ src/routes/gallery/index.svelte
@@ -4,29 +4,21 @@
   export async function load({ url }) {
     const { data, error } = await supabase.from("properties").select();
 
-    const loadedProperty = data.map((data, index) => {
+    const loadedimgs = data.map((data, index) => {
       return {
         type_desc: data.type_desc,
         image: `https://nphykmdyafqximjeemqm.supabase.in/storage/v1/object/public/images/${
           index + 1
         }.jpeg`,
-        offer_title: data.offer_title,
-        currency: data.currency,
+      
         id: index + 1,
-        area_size: data.area_size,
-        price: data.price,
-        bedrooms: data.bedrooms,
-        storeys: data.storeys,
-        floor: data.floor,
-        location_country: data.location_country,
-        location_city: data.location_city,
-        location_hood: data.location_hood,
+       
       };
     });
 
     if (error) throw new Error(error.message);
 
-    return { props: { data: loadedProperty } };
+    return { props: { data: loadedimgs } };
   }
 </script>
 
