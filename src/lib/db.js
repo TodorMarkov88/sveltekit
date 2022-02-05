6b035b2cf647f7196b99ad02b0bfe0abda065edf
Todor Markov <todor.markoww@gmail.com>
2022-02-05 14:50:40 +0200

    setup-env-v1.3

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,13 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
 
-// const supabaseUrl = 'https://nphykmdyafqximjeemqm.supabase.co'
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDE2MzE1MCwiZXhwIjoxOTU1NzM5MTUwfQ.-w1NRh4b-iPxprky42PiYQ211LewXV_hiIJ8GrB7kNY'
-// const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = 'https://nphykmdyafqximjeemqm.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDE2MzE1MCwiZXhwIjoxOTU1NzM5MTUwfQ.-w1NRh4b-iPxprky42PiYQ211LewXV_hiIJ8GrB7kNY'
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 
-const supabase = createClient(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_ANON_KEY
- )
+// const supabase = createClient(
+//     import.meta.env.VITE_SUPABASE_URL,
+//     import.meta.env.VITE_SUPABASE_ANON_KEY
+//  )
 export default supabase
