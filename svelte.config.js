b7e665fce608a4aee8791bd3fa3b68cd92753bce
Todor Markov <todor.markoww@gmail.com>
2022-02-05 14:39:01 +0200

    setup-env-v1.0

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,12 @@
-// import vercel from '@sveltejs/adapter-vercel'
-import adapter from '@sveltejs/adapter-auto'
+  import vercel from '@sveltejs/adapter-vercel'
+// import adapter from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
  
 	kit: {
-		adapter: adapter(),
-		vite: {
-			define: {
-			  '.env': process.env,
-			},
-		  },
+		adapter: vercel(),
+	 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	},
