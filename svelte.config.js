1c788f7ca9c4b63c9fb9e4121868b6626cf97073
Todor Markov <todor.markoww@gmail.com>
2022-02-06 10:03:43 +0200

    hide-supabase-auth-v1.6

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,12 @@ const config = {
 		adapter: vercel(),
 	 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+            ssr: {
+                noExternal: dev ? [] : ['@supabase/supabase-js']
+            },
+        }
 	},
 	transform: {
 		"^.+\\.svelte$": ["svelte-jester", { "preprocess": true }]
