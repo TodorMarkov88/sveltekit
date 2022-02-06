c86b9ccf9deb495441495467cddf70cd2bd34f41
Todor Markov <todor.markoww@gmail.com>
2022-02-06 09:48:42 +0200

    hide-supabase-auth-v1.2

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,13 +1,12 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-
-// const supabaseUrl = 'https://nphykmdyafqximjeemqm.supabase.co'
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDE2MzE1MCwiZXhwIjoxOTU1NzM5MTUwfQ.-w1NRh4b-iPxprky42PiYQ211LewXV_hiIJ8GrB7kNY'
+//  const supabaseUrl = 'https://nphykmdyafqximjeemqm.supabase.co'
+//   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDE2MzE1MCwiZXhwIjoxOTU1NzM5MTUwfQ.-w1NRh4b-iPxprky42PiYQ211LewXV_hiIJ8GrB7kNY'
 // const supabase = createClient(supabaseUrl, supabaseKey)
 
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
+// const supabase = createClient(supabaseUrl, supabaseKey)
 
-const supabase = createClient(
-    import.meta.env.VITE_PUBLIC_SUPABASE_URL,
-    import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY
- )
-export default supabase
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
