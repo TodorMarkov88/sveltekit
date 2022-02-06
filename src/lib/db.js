76d750ef43548dfdf668ea77cfa717bcb377ac8b
Todor Markov <todor.markoww@gmail.com>
2022-02-06 10:01:15 +0200

    hide-supabase-auth-v1.5

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,8 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 //   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDE2MzE1MCwiZXhwIjoxOTU1NzM5MTUwfQ.-w1NRh4b-iPxprky42PiYQ211LewXV_hiIJ8GrB7kNY'
 // const supabase = createClient(supabaseUrl, supabaseKey)
 
-const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 // const supabase = createClient(supabaseUrl, supabaseKey)
 
 const supabase = createClient(supabaseUrl, supabaseKey);
